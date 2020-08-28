d627a37ae7639912cdcb30b465fdf6f862974559
the-dimensionless <sumit.sssingh.singh45@gmail.com>
2020-08-28 07:59:54 +0530

    setting up redux2

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import HomeScreen from './components/HomeScreen';
+import HomeScreen from './views/HomeScreen';
 import EditPostScreen from './components/EditPostScreen';
 
 import { Provider } from 'react-redux';
---
+++ components/Post.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Post = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Post;
